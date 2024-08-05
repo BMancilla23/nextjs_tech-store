49f44c48a2b7fc896bc5cda9045c8031bb245d0b
bmancilla23 <bmancilla119@gmail.com>
2024-08-04 23:09:10 -0500

    feat: implementación de firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw--pzHn4E0CHx7f4nyNT2NH--MY3paLg",
+  authDomain: "tech-store-f27c3.firebaseapp.com",
+  projectId: "tech-store-f27c3",
+  storageBucket: "tech-store-f27c3.appspot.com",
+  messagingSenderId: "725203685333",
+  appId: "1:725203685333:web:62bd8d87334d6053baf75c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
